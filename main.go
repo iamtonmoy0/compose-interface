2cb4b1a6abdfd852ea7cb11d81694ffcc1976dd5
Tonmoy <tonmoy4671@gmail.com>
2026-06-19 16:29:11 +0600

    update

--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"bytes"
+	"crypto/sha1"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"io/ioutil"
+)
+
 func main() {
 
+	data := []byte("hello world")
+	hashAndBroadcast(bytes.NewReader(data))
+}
+
+func hashAndBroadcast(r io.Reader) error {
+	b, err := ioutil.ReadAll(r)
+	if err != nil {
+		return err
+	}
+	hash := sha1.Sum(b)
+	fmt.Println(hex.EncodeToString(hash[:]))
+	return broadcast(r)
+}
+func broadcast(r io.Reader) error {
+	b, err := ioutil.ReadAll(r)
+	if err != nil {
+		return err
+	}
+	fmt.Println("string of the byets:", string(b))
+	return nil
 }
